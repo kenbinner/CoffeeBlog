ccea6f115f810b2aa488f35a0b4e8dec3701ae06
kenbinner <kengo@binner.org>
2025-09-04 22:00:40 +0100

    Added basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Recipes } from './recipes/recipes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    component: Home,
+  },  {
+    path: 'recipes',
+    component: Recipes,
+  }
+];
